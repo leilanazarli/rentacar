93966f76a9e9fb62ce9c9494a3760ce18585fa13
leilanazarli <leyla.nezerli.2002@gmail.com>
2025-02-24 21:04:52 +0400

    chance js

--- js/login.js
+++ js/login.js
@@ -1,26 +1,26 @@
-document.addEventListener("DOMContentLoaded", function () {  // Səhifə tam yükləndikdən sonra icra olunur
-    let form = document.querySelector("form"); // Form elementi düzgün seçilir
-    let name = document.querySelector("#exampleInputEmail1");
-    let password = document.querySelector("#exampleInputPassword1");
+// document.addEventListener("DOMContentLoaded", function () {  // Səhifə tam yükləndikdən sonra icra olunur
+//     let form = document.querySelector("form"); // Form elementi düzgün seçilir
+//     let name = document.querySelector("#exampleInputEmail1");
+//     let password = document.querySelector("#exampleInputPassword1");
 
-    if (!form) {  // Əgər form tapılmırsa, xəta verməməsi üçün yoxlama
-        console.error("Form tapılmadı!");
-        return;
-    }
+//     if (!form) {  // Əgər form tapılmırsa, xəta verməməsi üçün yoxlama
+//         console.error("Form tapılmadı!");
+//         return;
+//     }
 
-    form.addEventListener("submit", function (e) {
-        e.preventDefault(); // Formun default göndərilməsini bloklayırıq
+//     form.addEventListener("submit", function (e) {
+//         e.preventDefault(); // Formun default göndərilməsini bloklayırıq
 
-        console.log(name.value, password.value); // Input dəyərlərini konsola yazdırırıq
+//         console.log(name.value, password.value); // Input dəyərlərini konsola yazdırırıq
 
-        axios.post('https://rentacar-api.onrender.com/host/login', {
-            name: name.value,   // Input dəyərlərini göndəririk
-            password: password.value
-        })
-        .then(response => {
-            console.log(response);
-            console.log(response.data);
-        })
-        .catch(error => console.error('Error:', error));
-    });
-});
+//         axios.post('https://rentacar-api.onrender.com/host/login', {
+//             name: name.value,   // Input dəyərlərini göndəririk
+//             password: password.value
+//         })
+//         .then(response => {
+//             console.log(response);
+//             console.log(response.data);
+//         })
+//         .catch(error => console.error('Error:', error));
+//     });
+// });
